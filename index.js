a0d2c53ed72767aaeb8e5e62830c75063ed452c4
Lakshay <50356418+Lakshay-Batra@users.noreply.github.com>
2020-04-10 21:08:59 +0530

    abc

--- index.js
+++ index.js
@@ -2,7 +2,3 @@ $("#a").on("click", () => {
     $("iframe").attr("src","matches-template.html");
     console.log("aclicked");
 });
-$("#b").on("click", () => {
-    $("iframe").attr("src","login.html");
-    console.log("bclicked");
-});
