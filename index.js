9a4dd1546931ecdd4864cfaf02aef7a8374260bf
Lakshay <50356418+Lakshay-Batra@users.noreply.github.com>
2020-04-11 16:23:39 +0530

    almost ready

--- index.js
+++ index.js
@@ -1,4 +1,24 @@
 $("#a").on("click", () => {
+    var $elem = $('body');
     $("iframe").attr("src","matches-template.html");
-    console.log("aclicked");
+    $('html, body').animate({scrollTop: $elem.height()}, 800);
+});
+$("#b").on("click", () => {
+    $("iframe").attr("src","new-and-online-template.html");
+});
+$("#c").on("click", () => {
+    $("iframe").attr("src","search-template.html");
+});
+$("#d").on("click", () => {
+    $("iframe").attr("src","interest-booster-template.html");
+});
+
+$("#f").on("click", () => {
+    $("iframe").attr("src","messages-template.html");
+});
+
+$("#profile").on("click", () => {
+    var $elem = $('body');
+    $("iframe").attr("src","profile-page-template.html");
+    $('html, body').animate({scrollTop: $elem.height()}, 800);
 });
